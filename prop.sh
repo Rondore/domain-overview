6ddb10a2154436cbf198a42194766c3a6072873c
Rondore <nipitmaster@gmail.com>
2025-02-22 00:45:22 -0500

    Remove DNS servers that are no longer public

--- prop.sh
+++ prop.sh
@@ -9,11 +9,8 @@ opendns=$(dig soa $domain @208.67.222.222 +short);
 norton=$(dig soa $domain @199.85.126.10 +short);
 nortonstrict=$(dig soa $domain @199.85.126.30 +short);
 comodo=$(dig soa $domain @8.26.56.26 +short);
-#cox=$(dig $domain soa @68.1.16.107 +short);
 verizon=$(dig soa $domain @4.2.2.1 +short);
-#comcast=$(dig soa $domain @75.75.76.76 +short);
 quest=$(dig soa $domain @205.171.3.26 +short);
-earthLink=$(dig soa $domain @207.69.188.185 +short);
 
 printf "Google (8.8.8.8):\n$google\n\n";
 printf "Cloudflare (1.1.1.1):\n$cloudflare\n\n";
@@ -21,9 +18,6 @@ printf "OpenDNS (208.67.222.222):\n$opendns\n\n";
 printf "Norton Secure (199.85.126.10):\n$norton\n\n";
 printf "Norton Strong Filter (199.85.126.30):\n$nortonstrict\n\n";
 printf "Comodo (8.26.56.26):\n$comodo\n\n";
-#printf "Cox (68.1.16.107):\n$cox\n\n";
 printf "Verizon (4.2.2.1):\n$verizon\n\n";
-#printf "Comcast (75.75.76.76):\n$comcast\n\n";
 printf "Century Link (205.171.3.26):\n$quest\n\n";
-printf "EarthLink (207.69.188.185):\n$earthLink\n\n";
-printf "Note that OpenDNS and Norton block security threats. Notron strong filter also blocks mature content, drugs, gambling, hate, sexual orientation, suicide, tobacco, and violence.\n\n"
+printf "Note that OpenDNS and Norton block security threats. Norton strong filter also blocks mature content, drugs, gambling, hate, sexual orientation, suicide, tobacco, and violence.\n\n"
